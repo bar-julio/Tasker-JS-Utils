6a902bd669e43718e0f11840675e300255833c42
Julio Barrios <jbarrios@sbfcorp.com>
2022-08-22 00:36:48 -0600

    New direction

--- db.js
+++ db.js
@@ -1,27 +1,130 @@
-let instrucion = "+temp.arr.secondary";
-let data = "2";
+let address = "temp.active";
+let data = "gh";
 let error = false;
 let message = "";
-console.log(instrucion);
-console.log(data);
+console.log("address: ", address);
+console.log("data: ", data);
 
+/* function readFile(params) {
+    let data = [
+        { active: true, data: "thisisdata", key: "ab" },
+        { active: true, data: "thisisdataoptinal", key: "cd" },
+        { active: true, data: "lastdata", key: "ef" },
+        { active: false, data: "thisisoff", key: "gh" },
+        { active: true, data: "thisison", key: "ij" },
+    ];
+    return JSON.stringify(data);
+}
+function writeFile(params, data) {
+    console.log("fileName: ", params);
+    console.log("data: ", data);
+    return undefined;
+} */
+
+let result = "None";
+let results = [];
+let multiple = false;
 try {
-    let regex = /^([a-z0-9])+(\.([a-z0-9]+|\[[0-9]+\])){0,1}(\.\[\](\.[a-z0-9]+)?)?$/;
-    let op = data == "-" ? "delete" : data && data != "" ? "update" : "read";
+    let regex = /^([a-z0-9]+)(\.(all|get|add|del|delete|upd|update|active))$/;
 
-    if (regex.test(instrucion)) {
-        let inst = instrucion.split(".");
-        let fileName = inst[0] + ".json";
-        let file = readFile(fileName);
-
-        let jsonRaw = JSON.parse(file);
-
+    if (regex.test(address)) {
+        let arr = address.split(".");
+        let fileName = arr[0];
+        let operation = arr[1];
+        let secondary = undefined;
+        if (operation == "upd" || operation == "update" || operation == "add") {
+            if (/^.*\|.*$/.test(data)) {
+                let arrData = data.split("|");
+                data = arrData[0];
+                secondary = arrData[1];
+            } else {
+                throw new Error("Data not ready");
+            }
+        }
+        let rawData = readFile(fileName + ".json");
+        let parsed = JSON.parse(rawData);
         let temp;
-        let
-        for (let i = 1; i < inst.length; i++) {
-
+        switch (operation) {
+            case "all":
+                let first = true;
+                for (const iterator of parsed) {
+                    if (iterator["active"]) {
+                        if (!first) {
+                            temp += "|" + iterator["data"];
+                            multiple = true;
+                        } else {
+                            first = false;
+                            temp = iterator["data"];
+                        }
+                        results.push(iterator["data"]);
+                    }
+                }
+                result = temp;
+                break;
+            case "get":
+                for (const iterator of parsed) {
+                    if (iterator["active"] && iterator["key"] == data) {
+                        result = iterator["data"];
+                        results.push(iterator["data"]);
+                        break;
+                    }
+                }
+                break;
+            case "add":
+                let node = { active: true, key: data, data: secondary };
+                if (parsed) temp = parsed.push(node);
+                else temp = [].push(node);
+                writeFile(fileName, JSON.stringify(parsed));
+                break;
+            case "del":
+            case "delete":
+                if (parsed) {
+                    for (let iterator of parsed) {
+                        if (iterator["active"] && iterator["key"] == data) {
+                            iterator["active"] = false;
+                            break;
+                        }
+                    }
+                    writeFile(fileName, JSON.stringify(parsed));
+                }
+                break;
+            case "upd":
+            case "update":
+                if (parsed) {
+                    for (let iterator of parsed) {
+                        if (iterator["active"] && iterator["key"] == data) {
+                            iterator["data"] = secondary;
+                            break;
+                        }
+                    }
+                    writeFile(fileName, JSON.stringify(parsed));
+                }
+                break;
+            case "active":
+                if (parsed) {
+                    for (let iterator of parsed) {
+                        if (!iterator["active"] && iterator["key"] == data) {
+                            iterator["active"] = true;
+                            break;
+                        }
+                    }
+                    writeFile(fileName, JSON.stringify(parsed));
+                }
+                break;
+            default:
+                result = "None";
+                break;
         }
     } else {
-        throw Exception("Instruction unrecognized");
+        throw new Error("Instruction unrecognized");
     }
-} catch (error) {}
+} catch (ex) {
+    error = true;
+    message = ex;
+} finally {
+    console.log("error: ", error);
+    console.log("message: ", message);
+    console.log("multiple: ", multiple);
+    console.log("result: ", result);
+    console.log("results: ", results);
+}
